dca0f59b1dd7716b4aa937c783af7a40aad9f4b1
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-07-02 22:54:21 -0300

    chore(usuarios): desativando constante de usuarios

--- src/app/data/constants/usuarios.ts
+++ src/app/data/constants/usuarios.ts
@@ -1,30 +1,30 @@
-import { Usuario } from "@/core/model/Usuario";
+// import { Usuario } from "@/core/model/Usuario";
 
-const data_usuarios: Usuario[] = [
-  {
-    id: "1",
-    email: "teste1@teste.com",
-    nome: "carlos1",
-    senha: "123456"
-  },
-  {
-    id: "2",
-    email: "teste2@teste.com",
-    nome: "carlos2",
-    senha: "123456"
-  },
-  {
-    id: "3",
-    email: "teste3@teste.com",
-    nome: "carlos3",
-    senha: "123456"
-  },
-  {
-    id: "4",
-    email: "teste4@teste.com",
-    nome: "carlos4",
-    senha: "123456"
-  }
-]
+// const data_usuarios: Usuario[] = [
+//   {
+//     id: "1",
+//     email: "teste1@teste.com",
+//     nome: "carlos1",
+//     senha: "123456"
+//   },
+//   {
+//     id: "2",
+//     email: "teste2@teste.com",
+//     nome: "carlos2",
+//     senha: "123456"
+//   },
+//   {
+//     id: "3",
+//     email: "teste3@teste.com",
+//     nome: "carlos3",
+//     senha: "123456"
+//   },
+//   {
+//     id: "4",
+//     email: "teste4@teste.com",
+//     nome: "carlos4",
+//     senha: "123456"
+//   }
+// ]
 
-export default data_usuarios
+// export default data_usuarios
