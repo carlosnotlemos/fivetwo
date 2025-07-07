0248e89fdb80d14d6d4e927aa97be19cacab96d0
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-07-06 22:37:35 -0300

    reafact

--- src/app/(public)/home/PromoLista.tsx
+++ src/app/(public)/home/PromoLista.tsx
@@ -13,11 +13,11 @@ export default function PromoLista() {
 
   return (
     <section className="max-w-7xl mx-auto">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-0 lg:gap-6 justify-items-center">
+      <ul className="grid grid-cols-1 lg:grid-cols-3 gap-0 lg:gap-6 justify-items-center">
         {produtos.map(produto => (
-          <PromoCard key={produto.id} produto={produto} />
+          <li><PromoCard key={produto.id} produto={produto} /></li>
         ))}
-      </div>
+      </ul>
     </section>
   );
 }
