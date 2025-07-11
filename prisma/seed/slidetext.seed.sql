2570d0891a283417f3cc30d9b036531df9cb80db
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-07-10 21:46:43 -0300

    refatorando

---
+++ prisma/seed/slidetext.seed.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE slidetext RESTART IDENTITY;
+
+INSERT INTO slidetext (texto, icon) VALUES
+  ('Surpreenda no Dia dos Pais com camisetas exclusivas, criativas e inesquecíveis', '🎁'),
+  ('Estilo para o seu pai: escolha camisetas únicas e marcantes agora mesmo', '👕'),
+  ('Neste Dia dos Pais, presenteie com autenticidade, conforto e atitude', '🎉');
