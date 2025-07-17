bf795aeb095360165a638e71c9f4091183721e01
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-07-17 14:29:35 -0300

    seed

--- prisma/seed/beneficio.seed.sql
+++ prisma/seed/beneficio.seed.sql
@@ -1,6 +1,6 @@
 TRUNCATE TABLE beneficios RESTART IDENTITY;
 
-INSERT INTO beneficios (titulo, descricao, icon) VALUES
+INSERT INTO beneficios (titulo, descricao, icon, imagemurl) VALUES
   ('parcelamento', 'Parcele suas compras em até 10x sem juros no cartão.', 'IconCreditCard', 'https://i.postimg.cc/TYGfWxZX/produto1.webp'),
   ('ENTREGA RÁPIDA', 'Receba seus produtos com agilidade e segurança em todo o Brasil.', 'IconCubeSend', 'https://i.postimg.cc/TYGfWxZX/produto1.webp'),
   ('atendimento', 'Fale com a gente pelo WhatsApp ou Instagram a qualquer hora.', 'IconDeviceDesktop', 'https://i.postimg.cc/TYGfWxZX/produto1.webp');
