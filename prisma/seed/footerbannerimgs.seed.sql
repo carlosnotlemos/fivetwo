be68b0f9da2e6f04c4a682e9b435b54a169028b2
carlosnotlemos <carlossousa_lemos@outlook.com>
2025-07-24 22:25:28 -0300

    visualização simples

--- prisma/seed/footerbannerimgs.seed.sql
+++ prisma/seed/footerbannerimgs.seed.sql
@@ -1,5 +1,5 @@
 TRUNCATE TABLE footer_banner_imgs RESTART IDENTITY;
 
 INSERT INTO footer_banner_imgs (nome, imagemurl) VALUES
-  ('teste1', 'https://i.postimg.cc/tJBB1jnT/testecard.png'),
-  ('teste2', 'https://i.postimg.cc/tJBB1jnT/testecard.png');
+  ('teste1', 'https://i.postimg.cc/cH636T0T/banner1.png'),
+  ('teste2', 'https://i.postimg.cc/t4K4GjgX/banner2.jpg');
--- src/app/components/shared/HeaderBanner.tsx
+++ src/app/components/shared/HeaderBanner.tsx
@@ -2,7 +2,7 @@ export default function HeaderBanner(){
   return (
     <header className="w-full pb-10">
       <img
-        src="/bclaro.png"
+        src="https://i.postimg.cc/t4K4GjgX/banner2.jpg"
         alt="Descrição da imagem"
         className="w-full h-auto object-cover "
         style={{maxHeight: '675px' }}
